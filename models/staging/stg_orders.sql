95ee050c7ecddc539f682c83beb7b18f1295bb57
sundarajanprasanna02@gmail.com <sundarajanprasanna02@gmail.com>
2022-07-26 07:58:11 +0000

    chnages to "stg_orders"

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -15,8 +15,7 @@ country,
 --from raw_product
 category,
 productname,
-subcategory,
-ordersellingprice - ordercostprice as orderprofit
+subcategory
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
